3169084da6a3d9af50949de7794c52f933025204
mediv <mediv@mail.ru>
2024-05-07 08:13:03 +0300

    start page

--- config/settings.py
+++ config/settings.py
@@ -89,6 +89,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIR = (
     BASE_DIR / 'static',
 )
--- mailing/urls.py
+++ mailing/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.conf.urls.static import static
 
+from config import settings
 from mailing.apps import MailingConfig
+from mailing.views import HomeView
 
 app_name = MailingConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
