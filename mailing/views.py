7c8f19596ad6c4485b14f1df5753b4c2174ea144
mediv <mediv@mail.ru>
2024-07-10 17:36:55 +0300

    index

--- mailing/views.py
+++ mailing/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from mailing.forms import ClientForm
@@ -33,13 +34,24 @@ class ClientDetailView(LoginRequiredMixin, DetailView):
 class ClientCreateView(LoginRequiredMixin, CreateView):
     model = Client
     form = ClientForm
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
+
+    def form_valid(self, form):
+        client = form.save()
+        user = self.request.user
+        client.owner = user
+        client.save()
+        return super().form_valid(form)
 
 
 class ClientUpdateView(LoginRequiredMixin, UpdateView):
     model = Client
     form = ClientForm
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:client_list')
 
 
 class ClientDeleteView(LoginRequiredMixin, DeleteView):
     model = Client
-
+    success_url = reverse_lazy('mailing:client_list')
